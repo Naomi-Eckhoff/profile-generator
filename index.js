3ddb1b7e1ecbb6486cddfe638965c7c23cc89e31
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-11-25 19:52:03 -0600

    trying something weird with the index because I really don't want to fix it the right way

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-new Employee().getName();
+new Employee().managerInquirer();
+
+if (Employee.getRole() === 'Manager') {
+  //jump to manager js
+}
+if (Employee.getRole() === 'Engineer') {
+  // jump to engineer js
+}
+if (Employee.getRole() === 'Intern') {
+  // jump to engineer js
+}
