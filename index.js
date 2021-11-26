6b03cd79e42e2beb4d7310df9519c16243045464
Naomi-Eckhoff <nsuibhne@gmail.com>
2021-11-26 03:30:52 -0600

    added logic to index and added fs for later

--- index.js
+++ index.js
@@ -2,15 +2,115 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const inquirer = require('inquirer');
 
-new Employee().managerInquirer();
+const answers = {
+  name: 'name',
+  id: 'id',
+  email: 'email',
+  role: 'role'
+};
 
-if (Employee.getRole() === 'Manager') {
-  //jump to manager js
+function managerInquirer() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'name',
+      message: "Please enter the manager's name"
+    })
+    .then(({ name }) => {
+      answers.name = name;
+      answers.role = 'Manager';
+      sharedInquirer()
+    });
 }
-if (Employee.getRole() === 'Engineer') {
-  // jump to engineer js
+
+function nonManagerInquirer() {
+  inquirer
+    .prompt({
+      type: 'input',
+      name: 'name',
+      message: "Please enter the employee's name"
+    })
+    .then(({ name }) => {
+      answers.name = name;
+      sharedInquirer();
+    });
 }
-if (Employee.getRole() === 'Intern') {
-  // jump to engineer js
+
+function sharedInquirer() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Please enter their id'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter their email'
+      }
+    ])
+    .then(({ id, email }) => {
+      answers.id = id;
+      answers.email = email;
+      roleSplitter();
+    });
 }
+
+function roleSplitter() {
+  if (answers.role === 'Manager') {
+    new Manager(answers.name, answers.id, answers.email).managerInquirer();
+  }
+  if (answers.role === 'Engineer') {
+    new Engineer(answers.name, answers.id, answers.email).engineerInquirer();
+  }
+  if (answers.role === 'Intern') {
+    new Intern(answers.name, answers.id, answers.email).internInquirer();
+  }
+}
+
+function roleChoice() {
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'role',
+      message: 'Would you like to add another employee',
+      choices: ['Engineer', 'Intern', 'Finished']
+    })
+    .then(({ role }) => {
+      if (role === 'Finished') {
+        // jump to html write
+      }
+
+      answers.role = role;
+
+      nonManagerInquirer();
+    });
+}
+
+const writeToFile = content => {
+  //adds dist folder if it doesn't exist
+  if (!fs.existsSync('./dist')) {
+    fs.mkdirSync('./dist');
+  }
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/page.html', content, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created'
+      });
+    });
+  });
+};
+
+
+
+managerInquirer();
